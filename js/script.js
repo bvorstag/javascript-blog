fe8bb9a8d28ce48b818627158bbd35c3431590d0
Bartek Wokacz <bartek.wokacz666@gmail.com>
2022-11-17 17:46:08 +0100

    moduł 5.2 add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -27,9 +27,12 @@ function titleClickHandler(event){
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
  
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log()
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
