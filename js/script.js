ae15c590307c47e0c51bafbd515b1a9aeeb392a1
Bartek Wokacz <bartek.wokacz666@gmail.com>
2022-11-17 17:18:19 +0100

    get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
-
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-
+  /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
-
   const activeArticles = document.querySelectorAll('.posts .post.active');
     
   for (let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
     }
 
-  /* get 'href' attribute from the clicked link */
-
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+ 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
