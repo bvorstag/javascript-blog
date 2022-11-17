6b2e1c4584e356d2b00d345523df67313f376096
Bartek Wokacz <bartek.wokacz666@gmail.com>
2022-11-17 15:32:10 +0100

    remove class 'active' from all articles vol 2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.post');
-
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+    for (let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
   /* get 'href' attribute from the clicked link */
 
